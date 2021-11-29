94d3ae9a5a2d157ea3632a846a4b8af012c646f8
Tim Dugan <tadugan@me.com>
2021-11-29 17:28:13 -0600

    added TypeScript

---
+++ dist/App.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const jsx_runtime_1 = require("react/jsx-runtime");
+require("./App.css");
+function App() {
+    return ((0, jsx_runtime_1.jsx)("div", Object.assign({ className: "App" }, { children: (0, jsx_runtime_1.jsx)("header", Object.assign({ className: "App-header" }, { children: (0, jsx_runtime_1.jsxs)("p", { children: ["Edit ", (0, jsx_runtime_1.jsx)("code", { children: "src/App.js" }, void 0), " and save to reload."] }, void 0) }), void 0) }), void 0));
+}
+exports.default = App;
