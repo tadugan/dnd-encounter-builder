1eb3af07cd3f3c4355fbb86f3a521dc672577673
Tim Dugan <tadugan@me.com>
2021-11-30 14:32:41 -0600

    Added TailwindCSS and Craco

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,12 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div>
+
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    purge: ['./src/**/*.tsx', './src/**/*.js'], // TODO: Might need to add more routes
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
